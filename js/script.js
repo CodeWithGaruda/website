395ddf8f813e117d3e6a04259de2ef0543a228e0
cwg <cwg@gmail.com>
2022-05-16 13:06:10 +0530

    dynamic text

--- js/script.js
+++ js/script.js
@@ -1,6 +1,20 @@
-const toggleButton = document.getElementsByClassName('toggle-button')[0]
-const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+const toggleButton = document.getElementsByClassName("toggle-button")[0];
+const navbarLinks = document.getElementsByClassName("navbar-links")[0];
 
-toggleButton.addEventListener('click', () => {
-  navbarLinks.classList.toggle('active')
-})
+toggleButton.addEventListener("click", () => {
+  navbarLinks.classList.toggle("active");
+});
+
+let typed = new Typed(".dynamicText", {
+  strings: [
+    "TechTrainer",
+    "Youtuber",
+    "Java Devloper",
+    "Motivational Speaker",
+    "Programmer",
+    "Tech Enthusist",
+  ],
+  typeSpeed: 150,
+  backSpeed: 150,
+  loop: true,
+});
